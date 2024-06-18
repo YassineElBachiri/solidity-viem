2d82fc9c40534459dd600565ded7d4db4b984ec0
YASSINE EL BACHIRI <yassineelbachiri21@gmail.con>
2024-06-18 21:12:46 +0300

    test: eth tx tested

--- test/TokenSale.t.ts
+++ test/TokenSale.t.ts
@@ -40,66 +40,84 @@ async function fixture() {
 
 describe("NFT Shop", async () => {
   describe("When the Shop contract is deployed", async () => {
-    it.only("defines the ratio as provided in parameters", async () => {
-      const {tokenSaleContract} = await loadFixture(fixture);
+    it("defines the ratio as provided in parameters", async () => {
+      const { tokenSaleContract } = await loadFixture(fixture);
       const ratio = await tokenSaleContract.read.ratio();
       expect(ratio).to.eq(TEST_RATIO);
-      
     });
-    it.only("defines the price as provided in parameters", async () => {
-      const {tokenSaleContract} = await loadFixture(fixture);
+    it("defines the price as provided in parameters", async () => {
+      const { tokenSaleContract } = await loadFixture(fixture);
       const price = await tokenSaleContract.read.price();
       expect(price).to.eq(TEST_PRICE);
-      
     });
     it("uses a valid ERC20 as payment token", async () => {
-      
-     
+      const { tokenSaleContract } = await loadFixture(fixture);
+      const paymentTokenAddress = await tokenSaleContract.read.paymentToken();
+
+      const paymentTokenContract = await viem.getContractAt(
+        "ERC20",
+        paymentTokenAddress
+      );
+
+      await expect(paymentTokenContract.read.totalSupply()).to.be.not.rejected;
+
     });
     it("uses a valid ERC721 as NFT collection", async () => {
-      
+      const { tokenSaleContract, myNftContract } = await loadFixture(fixture);
+      await expect(myNftContract.read.name()).to.be.not.rejected;
+      await expect(myNftContract.read.balanceOf([myNftContract.address])).to.be.not.rejected;
+
     });
   });
   describe("When a user buys an ERC20 from the Token contract", async () => {
-    it("charges the correct amount of ETH", async () => {
-      
+    it.only("charges the correct amount of ETH", async () => {
+      const {tokenSaleContract, deployer, publicClient} = await loadFixture(fixture);
+
+      const deployerBalance = await publicClient.getBalance({
+        address: deployer.account.address,
+      });
+
+      const tx_hash = await tokenSaleContract.write.buyTokens({
+        value: parseEther(TEST_BUY_AMOUNT),
+        account: deployer.account.address
+      });
+
+      const tx_receipt = await publicClient.getTransactionReceipt({
+        hash:tx_hash,
+      });
+
+
+      // calculate tx fees
+      const gasAmount = tx_receipt.gasUsed;
+      const gasPrice = tx_receipt.effectiveGasPrice;
+      const txFees = gasAmount * gasPrice;
+
+
+      const deployerBalanceAfter = await publicClient.getBalance({
+        address: deployer.account.address,
+      });
+
+       // the difference should be: the initial amount - (amount bought + tx fees)
+       const diff = deployerBalance - deployerBalanceAfter;
+       const expected_diff = parseEther(TEST_BUY_AMOUNT) + txFees;
+       expect(diff).to.be.eq(expected_diff);
     });
 
-    it("gives the correct amount of tokens", async () => {
-   
-      
-    });
+    it("gives the correct amount of tokens", async () => {});
   });
   describe("When a user burns an ERC20 at the Shop contract", async () => {
-    it("gives the correct amount of ETH", async () => {
-     
-
-    });
-    it("burns the correct amount of tokens", async () => {
-      
-      
-
-    });
+    it("gives the correct amount of ETH", async () => {});
+    it("burns the correct amount of tokens", async () => {});
   });
   describe("When a user buys an NFT from the Shop contract", async () => {
-    it("charges the correct amount of ERC20 tokens", async () => {
-     
-    });
-    it("gives the correct NFT", async () => {
-     
-    });
+    it("charges the correct amount of ERC20 tokens", async () => {});
+    it("gives the correct NFT", async () => {});
   });
   describe("When a user burns their NFT at the Shop contract", async () => {
-    it("gives the correct amount of ERC20 tokens", async () => {
-     
-    });
+    it("gives the correct amount of ERC20 tokens", async () => {});
   });
   describe("When the owner withdraws from the Shop contract", async () => {
-    it("recovers the right amount of ERC20 tokens", async () => {
-      
-    });
-    it("updates the owner pool account correctly", async () => {
-      
-    });
+    it("recovers the right amount of ERC20 tokens", async () => {});
+    it("updates the owner pool account correctly", async () => {});
   });
 });
