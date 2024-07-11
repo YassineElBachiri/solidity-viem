670ee02ad48b9079fdce186bc07fe6336305085d
YASSINE EL BACHIRI <yassineelbachiri21@gmail.con>
2024-07-11 12:57:27 +0300

    chore : hardhat.config.ts cleaned

--- test/TokenSale.t.ts
+++ test/TokenSale.t.ts
@@ -111,6 +111,7 @@ describe("NFT Shop", async () => {
     const tokenBalanceBefore = await myTokenContract.read.balanceOf([
       acc1.account.address,
     ]);
+    // jksdjfks
  // ghghhghghg
     // acc 1 buys tokens nnn
     const tx = await tokenSaleContract.write.buyTokens({
